7bb6ff186410bb5c1dbd0cbfa252b35e5dc716a1
Aloke Pramanik <129755834+hackerbotsupreme@users.noreply.github.com>
2023-11-08 11:49:07 +0530

    Add files via upload

--- 20-filters/final/products.js
+++ 20-filters/final/products.js
@@ -3,28 +3,32 @@ const products = [
     id: 'rec43w3ipXvP28vog',
     title: 'high-back bench',
     company: 'ikea',
-    image: 'https://course-api.com/images/store/product-1.jpeg',
+    image:
+      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681749482/react-comfy-store-products/iuYyO9RP_o_upinxq.jpg',
     price: 9.99,
   },
   {
     id: 'rec4f2RIftFCb7aHh',
     title: 'albany table',
     company: 'marcos',
-    image: 'https://course-api.com/images/store/product-2.jpeg',
+    image:
+      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681750782/react-comfy-store-products/product-2_lusrzx.jpg',
     price: 79.99,
   },
   {
     id: 'rec8kkCmSiMkbkiko',
     title: 'accent chair',
     company: 'caressa',
-    image: 'https://course-api.com/images/store/product-3.jpeg',
+    image:
+      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681750874/react-comfy-store-products/product-3_znpiqa.jpg',
     price: 25.99,
   },
   {
     id: 'recBohCqQsot4Q4II',
     title: 'wooden table',
     company: 'caressa',
-    image: 'https://course-api.com/images/store/product-4.jpeg',
+    image:
+      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681750929/react-comfy-store-products/product-4_ebl6q1.jpg',
 
     price: 45.99,
   },
@@ -32,7 +36,8 @@ const products = [
     id: 'recDG1JRZnbpRHpoy',
     title: 'dining table',
     company: 'caressa',
-    image: 'https://course-api.com/images/store/product-5.jpeg',
+    image:
+      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681751026/react-comfy-store-products/product-5_n184nu.jpg',
 
     price: 6.99,
   },
@@ -40,49 +45,56 @@ const products = [
     id: 'recNWGyP7kjFhSqw3',
     title: 'sofa set',
     company: 'liddy',
-    image: 'https://course-api.com/images/store/product-6.jpeg',
+    image:
+      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681752461/react-comfy-store-products/product-6_rhaxfo.jpg',
     price: 69.99,
   },
   {
     id: 'recZEougL5bbY4AEx',
     title: 'modern bookshelf',
     company: 'marcos',
-    image: 'https://course-api.com/images/store/product-7.jpeg',
+    image:
+      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681753280/react-comfy-store-products/prod-7_ta21yy.jpg',
     price: 8.99,
   },
   {
     id: 'recjMK1jgTb2ld7sv',
     title: 'emperor bed',
     company: 'liddy',
-    image: 'https://course-api.com/images/store/product-8.jpeg',
+    image:
+      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681753314/react-comfy-store-products/prod-8_qzulqi.jpg',
     price: 21.99,
   },
   {
     id: 'recmg2a1ctaEJNZhu',
     title: 'utopia sofa',
     company: 'marcos',
-    image: 'https://course-api.com/images/store/product-9.jpeg',
+    image:
+      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681753354/react-comfy-store-products/prod-9_j3dsja.jpg',
     price: 39.95,
   },
   {
     id: 'recvKMNR3YFw0bEt3',
     title: 'entertainment center',
     company: 'liddy',
-    image: 'https://course-api.com/images/store/product-10.jpeg',
+    image:
+      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681753394/react-comfy-store-products/prod-10_d0jqoh.jpg',
     price: 29.98,
   },
   {
     id: 'recxaXFy5IW539sgM',
     title: 'albany sectional',
     company: 'ikea',
-    image: 'https://course-api.com/images/store/product-11.jpeg',
+    image:
+      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681753441/react-comfy-store-products/prod-11_lff6yt.jpg',
     price: 10.99,
   },
   {
     id: 'recyqtRglGNGtO4Q5',
     title: 'leather sofa',
     company: 'liddy',
-    image: 'https://course-api.com/images/store/product-12.jpeg',
+    image:
+      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681753501/react-comfy-store-products/prod-12_ga6sdq.jpg',
     price: 9.99,
   },
 ];
